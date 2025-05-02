6bd416a7af406920330f5b40ceeac38b25aa122c
mukeshr-29 <mukeshr2911@gmail.com>
2025-05-02 20:28:40 +0530

    modified to war

--- src/main/frontend/vite-project/vite.config.js
+++ src/main/frontend/vite-project/vite.config.js
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,43 +9,21 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // CHANGED/ADDED - START
   build: {
-    // outDir: '../../resources/static/vite-project',
-    outDir: '../../../../target/classes/static/vite-project',
+    outDir: '../../../../target/classes/static/vite-project', // Kept as is
     emptyOutDir: true
   },
-  base: '/vite-project/',
+  base: '/v/', // Changed to match Vue Router and deployment
   define: {
-    'process.env.VUE_ROUTER_BASE': '"/v/"'
+    'process.env.VUE_ROUTER_BASE': '"/v/"' // Correct
   },
   server: {
-    // open: '/v/',
     open: true,
     proxy: {
-      // this works, but what if you have hundreds of different backend URLs?
-      // '/api': {
-      //   target: 'http://localhost:8080',
-      //   changeOrigin: true
-      // }
-      // first try like in vue.config.js - destroys live reload!
-      // '^.*': {
-      //   target: 'http://localhost:8080',
-      //   changeOrigin: true,
-      //   ws: false,
-      //   bypass: function (req, res, options) {
-      //     if (req.url.startsWith('/v/')) {
-      //       return req.url
-      //     }
-      //   }
-      // }
-      // second try - does also not work:
-      // '^^(?!/v/).*$': {
-      //   target: 'http://localhost:8080',
-      //   changeOrigin: true
-      //   ws: false,
-      // }
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
     }
   }
-  // CHANGED/ADDED - END
 })
--- src/main/frontend/vue-cli-project/vue.config.js
+++ src/main/frontend/vue-cli-project/vue.config.js
@@ -1,36 +1,27 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
+const { defineConfig } = require('@vue/cli-service');
 
-const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  // CHANGED/ADDED - START
-  //outputDir: "../../resources/static/vue-cli-project",
-  outputDir: "../../../../target/classes/static/vue-cli-project",
-  publicPath: "/vue-cli-project/",
+  outputDir: '../../../../target/classes/static/vue-cli-project',
+  publicPath: '/w/',
   devServer: {
-    open: "/w/",
+    open: '/w/',
     proxy: {
-      ".*": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        ws: false,
-        bypass: function (req, res, proxyOptions) {
-          if (req.url.startsWith("/w/")) {
-            return req.url;
-          }
-        },
-      },
-    },
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
   },
-  // configureWebpack: {
-  //   plugins: [
-  //     new webpack.DefinePlugin({
-  //       "process.env": {
-  //         VUE_ROUTER_BASE: '"/w/"',
-  //       },
-  //     }),
-  //   ],
-  // },
-  // CHANGED/ADDED - END
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          VUE_ROUTER_BASE: '"/w/"'
+        }
+      })
+    ]
+  }
 });
